35954afb84b52257b767f91d601e166156d3552a
Nvh270704 <150144498+Nvh270704@users.noreply.github.com>
2023-12-17 20:50:39 +0100

    Update interpreter.c

--- interpreter.c
+++ interpreter.c
@@ -1,4 +1,4 @@
-#include "parser.h" // Include your parser header
+#include "parser.h" 
 
 // Function to interpret an AST node
 void interpretAST(ASTNode *node) {
@@ -13,18 +13,15 @@ void interpretAST(ASTNode *node) {
             if (node->data.displayExpression->type == AST_TEXT) {
                 printf("%s\n", node->data.displayExpression->data.text);
             }
-            // Add handling for other types of expressions
+            
             break;
-        // Add cases for other AST node types
+        
     }
 
-    // Recursively interpret child nodes
-    // ...
+
 }
 
 int main() {
-    // Example usage
-    // 1. Tokenize and parse your source code to get an AST
-    // 2. Call interpretAST on the root of the AST
+
     return 0;
 }
